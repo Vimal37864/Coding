4a1a68f928741aed72114165c0dc02c291797291
Vimal37864 <vimalbalaji787@gmail.com>
2026-05-12 14:43:46 +0530

    Add files via upload

---
+++ Construct.java
@@ -0,0 +1,16 @@
+package Construct.java;
+
+class Sample {
+    int rollno;
+    int marks;
+}
+class construct {
+    public static void main() {
+        Sample odj = new Sample();
+        System.out.println(obj.marks);
+    }
+
+    static class AccessMod {
+    
+    }
+}
