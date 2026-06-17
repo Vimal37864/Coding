a10e00bd0afe02dea2950e28482e3317815464f0
Vimal37864 <vimalbalaji787@gmail.com>
2026-06-17 21:45:51 +0530

    Add files via upload

---
+++ LargestAltitude.java
@@ -0,0 +1,7 @@
+class Solution {
+    public int largestAltitude(int[] gain) {
+        int[] prefixsum = new int[gain.length + 1];
+        prefixsum[0] = 0;
+        for (int i = 1; i <= gain.length; i++) {
+            prefixsum[i] = prefixsum[i - 1] + gain[i - 1];
+        }
