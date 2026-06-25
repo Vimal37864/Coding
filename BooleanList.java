dc9d07cd633c086bb3db98629551355084cc9088
Vimal37864 <vimalbalaji787@gmail.com>
2026-06-25 22:27:29 +0530

    Add files via upload

---
+++ BooleanList.java
@@ -0,0 +1,11 @@
+class Solution {
+    public List<Boolean> kidsWithCandies(int[] candies, int extraCandies) {
+        List<Boolean> l=new ArrayList<>();
+        int max=0;
+        for(int i:candies) max=Math.max(max,i);
+        for(int i=0;i<candies.length;i++){
+            l.add(candies[i]+extraCandies >= max);
+        }
+        return l;
+    }
+}
